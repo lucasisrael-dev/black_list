a76b8291dc2250f62da60c1fa9486bfcf0d2f46b
Lucas Israel <lucasisrael.dev@gmail.com>
2025-12-09 07:54:14 -0400

    Commit automático via PowerShell - 09-12-2025_07-52-32

--- create_blacklist.ps1
+++ create_blacklist.ps1
@@ -6,7 +6,7 @@ $today = Get-Date -Format "dd-MM-yyyy_hh-mm-ss"
 #$outFile = "C:\Users\lucasisrael\Documents\scripts\logs\ips-$today.csv"
 
 # Define the time range (last 24 hours)
-$startTime = (Get-Date).AddHours(-24)
+$startTime = (Get-Date).AddHours(-48)
 
 Write-Host "Searching last $MaxEventsToSearch Failed Logon events (ID 4625)..." -ForegroundColor Cyan
 
